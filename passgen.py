9cf5e53c71d660e2ab4a2e469556ab5b7187c8d3
Beau Brooks <76848623+vvixi@users.noreply.github.com>
2023-06-08 23:04:00 -0600

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -3,8 +3,9 @@ import random
 import string
 import os
 import time
+from argparse import ArgumentParser
 
-def makePassword(length, num=False, strength='weak') -> str:
+def makePassword(length=10, num=False, strength='weak'):
 	'''length of password, num if you want a number,
     and strenth: weak, medium, strong'''
 	lower = string.ascii_lowercase
@@ -24,8 +25,8 @@ def makePassword(length, num=False, strength='weak') -> str:
 
 	elif strength == 'medium':
 		if num:
-			length -= 2
-			for i in range(2):
+			length -= 4
+			for i in range(4):
 				pwd += random.choice(dig)
 		for i in range(length):
 			pwd += random.choice(letter)
@@ -44,16 +45,40 @@ def makePassword(length, num=False, strength='weak') -> str:
 
 	pwd = list(pwd)
 	random.shuffle(pwd)
-	return ''.join(pwd)
+	password = ''.join(pwd)
+	print("Success")
+	return password
 
-def displayPassword():
+def displayPassword(_length, _num, _strength):
+    
+    while int(_length) < 6:
+        choice = input("Please chose a number greater than 5: ")
+        _length = choice
 
-    print(makePassword(15,num=True))
-    print(makePassword(20,num=True, strength="medium"))
-    print(makePassword(29, strength="strong"))
-    time.sleep(8)
-    os.system('cls' if os.name == 'nt' else 'clear')
+    password = makePassword(int(_length), _num, _strength)
+    answer = input("Display password? ")
 
-answer = input("Make password? ")
-if answer == 'yes' or answer == 'YES' or answer == 'Yes':
-    displayPassword()
+    if answer == 'yes' or answer == 'YES' or answer == 'Yes':
+        
+        print(password)
+        time.sleep(8)
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    elif answer == 'no' or answer == 'NO' or answer == 'No':
+        print("Thank you for using PassGen")
+        return
+
+if __name__ == '__main__':
+    # create the argparse instance
+    parser = ArgumentParser(
+                prog = 'PassGen',
+                description = 'Easily create complex strings and passwords')
+    parser.add_argument("-l", "--length", dest="length", help="length of password")
+    
+    parser.add_argument("-n", "--numbers", dest="num", help="contains numbers")
+
+    parser.add_argument("-s", "--strength", dest="strength", help="specify strength of password")
+
+    args = parser.parse_args()
+
+    displayPassword(args.length, args.num, args.strength)
